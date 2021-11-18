4b602677f45b33806bcd7978847b889dc4ce8a00
zhog96 <43547995+zhog96@users.noreply.github.com>
2021-11-18 05:08:16 +0300

    ansible

---
+++ java/src/test/java/countdown/MainTest.java
@@ -0,0 +1,19 @@
+package countdown;
+
+import org.junit.jupiter.api.Test;
+
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class MainTest {
+
+    @Test
+    void main() {
+        final ByteArrayOutputStream stream = new ByteArrayOutputStream();
+        System.setOut(new PrintStream(stream));
+        new Countdown(10).run();
+        assertEquals("0\n1\n2\n3\n4\n5\n6\n7\n8\n9\n", stream.toString());
+    }
+}
